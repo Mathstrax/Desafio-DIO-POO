e456da9ddd8b1ff945441ccdd380816406acec63
Matheus De Alencar <mathstrax@gmail.com>
2022-01-27 13:36:29 -0300

    Desafio Bootcamp DIO

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+    Curso curso1 = new Curso();
+    curso1.setTitulo("curso java");
+    curso1.setDescricao("descrição curso java");
+    curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("O dev vai aprender na prática como desenvolver utilizando a IDE Javascript");
+        curso2.setCargaHoraria(56);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Nessa mentoria o dev ira tirar as dúvidas e realizar desafios");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
